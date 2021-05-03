663689fa16b7cd22fcccad50274d331be33665da
kim <kim@DESKTOP-N9MU2BT>
2021-05-03 23:40:01 +0900

    반복문을 통한 계산 처리

--- Calculator/src/calculator.java
+++ Calculator/src/calculator.java
@@ -3,58 +3,78 @@ import java.util.Scanner;
 public class calculator {
 
 	public static void main(String[] args) {
-		System.out.println("하고싶은 사칙연산을 입력하세요. : ");
-		Scanner scanner = new Scanner(System.in);
-		String str = scanner.next();
-		
-		if (!str.equals("+") && !str.equals("-") && !str.equals("*") && !str.equals("/")  ) {
-			System.out.println("사칙연산중 하나를 입력하세요. ");
-		}else {
-			System.out.println(str + "를 진행할 첫번째 숫자를 입력하세요.");
-		}
+		String str 		= "";
+		int secondNum 	= 0;
+		int result 		= 0;
 
-		Scanner input1 = new Scanner(System.in);
-//		int firstNum = input1.nextInt(); 
+		System.out.println("정수를 입력하세요. : ");
 		
+		Scanner input1 = new Scanner(System.in);
 		while (!input1.hasNextInt()) {
 			input1.next();
 			System.err.print("정수를 입력하세요. ");
 		}
-		int firstNum = input1.nextInt();
-
-		System.out.println("두번째 값을 입력하세요. : ");
-		Scanner input2 = new Scanner(System.in);
+		result = input1.nextInt();
 		
-		// 숫자만 입력가능하게
-		while(!input2.hasNextInt()) {
-			input2.next();
-			System.err.print("정수를 입력하세요.");
+		while (true) {
+			System.out.println("사칙연산중 하나를 입력하세요. ");
+			Scanner scanner = new Scanner(System.in);
+			str = scanner.next();
+			
+			//"quit" 입력시 탈출
+			if (str.equals("quit")) {
+				System.out.println("계산값은 : "+result);
+				break;
+			}
+			
+			//사칙연산 입력받을때까지 반복
+			if (!str.equals("+") && !str.equals("-") && !str.equals("*") && !str.equals("/")  ) {
+				System.out.println("사칙연산중 하나를 입력하세요. ");
+
+			//정수를 입력받아서 계산
+			}else {
+				System.out.println("정수를 입력하세요.");
+
+				Scanner input2 = new Scanner(System.in);
+				while (!input2.hasNextInt()) {
+					input2.next();
+					System.err.print("정수를 입력하세요. ");
+				}
+				secondNum = input2.nextInt();
+				
+				if (str.equals("+")) {
+					result = Plus(result, secondNum);
+
+				}else if(str.equals("-")) {
+					result = Minus(result, secondNum);
+
+				}else if(str.equals("*")) {
+					result = Multiplication(result, secondNum);
+
+				}else if(str.equals("/")) {
+					result = Division(result, secondNum);
+				}
+			}
 		}
-		
-		int secondNum = input2.nextInt();
-		int result = 0;
-		
-		if (str.equals("+")) {
-			result = firstNum - secondNum;
-
-		}else if(str.equals("-")) {
-			result = firstNum - secondNum;
-
-		}else if(str.equals("*")) {
-			result = firstNum * secondNum;
-
-		}else if(str.equals("/")) {
-			result = firstNum / secondNum;
-		}
-
-		System.out.println(firstNum + str + secondNum + " = " + result);
 	}
 	
-	// 숫자 체크
-//	public boolean isNum(String input)
-//	{
-//	 var chars = "0123456789";
-//	 return containsCharsOnly(input, chars);
-//	}
-//
+	//덧셈
+	public static int Plus(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	//뺄셈
+	public static int Minus(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	//곱셈
+	public static int Multiplication(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	//나눗셈
+	public static int Division(int num1, int num2) {
+		return num1 / num2;
+	}
 }
