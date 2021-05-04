7baf60e8f827b5f0ba0cf1e61a90d1834d4f5c3c
kim <kim@DESKTOP-N9MU2BT>
2021-05-05 00:07:02 +0900

    3-5 메소드를 분리해 계산기 기능 구현

--- Calculator/src/calculator.java
+++ Calculator/src/calculator.java
@@ -7,74 +7,100 @@ public class calculator {
 		int secondNum 	= 0;
 		int result 		= 0;
 
-		System.out.println("정수를 입력하세요. : ");
-		
 		Scanner input1 = new Scanner(System.in);
-		while (!input1.hasNextInt()) {
-			input1.next();
-			System.err.print("정수를 입력하세요. ");
-		}
-		result = input1.nextInt();
+		result = getFirstValue(input1);	//첫번째 정수 가져오기
 		
 		while (true) {
-			System.out.println("사칙연산중 하나를 입력하세요. ");
-			Scanner scanner = new Scanner(System.in);
-			str = scanner.next();
+			Scanner arithmetics = new Scanner(System.in);
+			str = getSymbol(arithmetics);	//사칙연산 가져오기
 			
 			//"quit" 입력시 탈출
 			if (str.equals("quit")) {
-				System.out.println("계산값은 : "+result);
+				System.out.println("탈출!");
 				break;
 			}
 			
 			//사칙연산 입력받을때까지 반복
 			if (!str.equals("+") && !str.equals("-") && !str.equals("*") && !str.equals("/")  ) {
-				System.out.println("사칙연산중 하나를 입력하세요. ");
 
 			//정수를 입력받아서 계산
 			}else {
-				System.out.println("정수를 입력하세요.");
-
 				Scanner input2 = new Scanner(System.in);
-				while (!input2.hasNextInt()) {
-					input2.next();
-					System.err.print("정수를 입력하세요. ");
-				}
-				secondNum = input2.nextInt();
+				secondNum = getSecondValue(input2);		//두번째 정수 가져오기
 				
-				if (str.equals("+")) {
-					result = Plus(result, secondNum);
+				result = calculate(result, str, secondNum);
 
-				}else if(str.equals("-")) {
-					result = Minus(result, secondNum);
-
-				}else if(str.equals("*")) {
-					result = Multiplication(result, secondNum);
-
-				}else if(str.equals("/")) {
-					result = Division(result, secondNum);
-				}
+				print(result);
 			}
 		}
 	}
 	
+	
+	static int getFirstValue(Scanner scanner) {
+		System.out.println("정수를 입력하세요.");
+		
+		while (!scanner.hasNextInt()) {
+			scanner.next();
+			System.err.print("정수를 입력하세요. ");
+		}
+		return scanner.nextInt();
+	}
+	
+	static String getSymbol(Scanner scanner) {
+		System.out.println("사칙연산중 하나를 입력하세요. ");
+		return scanner.next();
+	}
+
+	static int getSecondValue(Scanner scanner) {
+		System.out.println("정수를 입력하세요.");
+		
+		while (!scanner.hasNextInt()) {
+			scanner.next();
+			System.err.print("정수를 입력하세요. ");
+		}
+		return scanner.nextInt();
+	}
+	
+	static int calculate(int input1, String symbol, int input2) {
+		int result = 0;
+
+		if (symbol.equals("+")) {
+			result = Plus(input1, input2);
+
+		}else if(symbol.equals("-")) {
+			result = Minus(input1, input2);
+
+		}else if(symbol.equals("*")) {
+			result = Multiplication(input1, input2);
+
+		}else if(symbol.equals("/")) {
+			result = Division(input1, input2);
+		}	
+		
+		return result;
+	}
+	
+	static void print(int cal) {
+		System.out.println("계산값은 : " + cal);
+	}
+	
 	//덧셈
-	public static int Plus(int num1, int num2) {
+	static int Plus(int num1, int num2) {
 		return num1 + num2;
 	}
 
 	//뺄셈
-	public static int Minus(int num1, int num2) {
+	static int Minus(int num1, int num2) {
 		return num1 - num2;
 	}
 
 	//곱셈
-	public static int Multiplication(int num1, int num2) {
+	static int Multiplication(int num1, int num2) {
 		return num1 * num2;
 	}
 
 	//나눗셈
-	public static int Division(int num1, int num2) {
+	static int Division(int num1, int num2) {
 		return num1 / num2;
 	}
 }
